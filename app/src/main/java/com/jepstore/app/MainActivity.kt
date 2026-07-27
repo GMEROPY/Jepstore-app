cc17dd2ae1c6a4c996ed851e748148451bf2dad0
GMEROPY <oktaaditya286@gmail.com>
2026-07-27 12:55:48 +0700

    Create app/src/main/java/com/jepstore/app/MainActivity.kt

---
+++ app/src/main/java/com/jepstore/app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.jepstore.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.loadUrl("https://jepstore.my.id/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
